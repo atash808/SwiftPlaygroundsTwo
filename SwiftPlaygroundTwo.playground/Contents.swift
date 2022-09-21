7f74c2a8efe9bcaaf3cdc85acf6113169560aa9d
atash808 <atash@hawaii.edu>
2022-09-21 11:25:23 -1000

    Completed Part 5: On your own

--- SwiftPlaygroundTwo.playground/Contents.swift
+++ SwiftPlaygroundTwo.playground/Contents.swift
@@ -22,4 +22,37 @@ if gradeA == true
     print("I'm not so sure about coding in Swift")
 
 }
+//Part 5
+//Problem Set 1
+let number = -2
+if (number > 0) {
+    print("Number is positive")
+}
+
+else {
+    print("Number is negative")
+}
+//Problem Set 2
+var action: String
+var animal = "dolphin"
+
+if animal == "dolphin" {
+    action = "ocean animal"
+} else if animal == "dog" {
+            action = "land animal"
+}
+//Problem Set 3
+var i = 5
+if(i>10)
+{
+    print("i is bigger than 10")
+}
+else
+{
+    print("i is not bigger than 10")
+}
+
+
+
+
 
